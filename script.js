014ee885d0a425405dea1c9209376d1fd4b52abd
wahid7ark <wahid.ark7330@gmail.com>
2026-02-28 01:20:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function startMusic(){
+function startMusic() {
     var audio = document.getElementById("bgmusic");
-    if(audio){
-        audio.play().catch(()=>{});
+
+    if (audio) {
+        audio.play().catch(function () {});
     }
 }
-document.addEventListener("click", startMusic, {once:true});
-document.addEventListener("touchstart", startMusic, {once:true});
+
+document.addEventListener("click", startMusic, { once: true });
+document.addEventListener("touchstart", startMusic, { once: true });
